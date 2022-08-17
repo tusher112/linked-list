9c4f20eb352875519c2f7a6c6cfa8745ad79e270
Md. Tusher Ahmed Aman <54816317+tusher112@users.noreply.github.com>
2022-08-18 00:02:47 +0600

    insert at Head in linear Linked list

--- linked list.cpp
+++ linked list.cpp
@@ -6,22 +6,59 @@ class Node
 	public:
 	int value;
 	Node *Next;
+	
+	//Construction creation
+	Node(int val){
+		value=val;
+		Next = NULL;
+	}
 };
+
+
+void insertAtTail(Node* &head,int val ){
+	Node *newNode = new Node(val);
+	if(head==NULL){
+		head=newNode;
+		return;
+	}
+	Node *temp= head;
+	while(temp->Next != NULL){
+		temp= temp->Next;
+	}
+	temp->Next=newNode;
+}
+
 
 void display(Node* n){
 	while(n !=NULL){
 		cout<<n->value;
 		
 		if(n->Next != NULL){
-			 cout<<" n-> ";
+			 cout<<" -> ";
 		}
 		n=n->Next;
 	}
 }
 
 int main(){
-	Node* head= new Node();
-	Node* second= new Node();
+	Node* head= NULL;
+	int n;
+	
+	char choice= 'Y';
+	
+	while(choice=='Y'){
+		cout<<"Please enter the value ";
+		cin>>n;
+		
+		insertAtTail(head, n);
+		cout<<"Do you want to continue: Y/N ";
+		cin>>choice;
+	}
+	
+	
+	//insertAtTail(head, 2);
+	//insertAtTail(head, 3);
+	/*Node* second= new Node();
 	Node* third= new Node();
 	Node* fourth= new Node();
 	
@@ -33,7 +70,7 @@ int main(){
 	head->Next=second;
 	second->Next=third;
 	third->Next=fourth;
-	fourth->Next=NULL;
+	fourth->Next=NULL; */
 	
 	display(head);
 	
